c6627771b6c30b3b2eabc098d41f719960492592
T0XHL <T0XHL@T0XHL-PC>
2025-05-30 17:49:45 -0500

    ola

--- src/main/java/com/mycompany/proyecto_edd/Proyecto_Edd.java
+++ src/main/java/com/mycompany/proyecto_edd/Proyecto_Edd.java
@@ -13,5 +13,6 @@ public class Proyecto_Edd {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Morning especial!");
+        System.out.println("PREPARAD VUESTROS ARTENTOXX");
     }
 }
