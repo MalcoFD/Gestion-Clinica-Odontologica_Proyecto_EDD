d34e7b30bf77f757683b59646fceb12f3f7fa83a
Malco <malcoferdiaz@gmail.com>
2025-05-30 17:37:58 -0500

    Getters y setters del paciente

---
+++ src/main/java/com/mycompany/proyecto_edd/Proyecto_Edd.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_edd;
+
+/**
+ *
+ * @author Juan
+ */
+public class Proyecto_Edd {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
